cd8d076519ee9ede7a7ef9a9d6b267d08a3b29f8
vistex-greyson <michelle.greyson@vistex.com>
2023-04-17 11:18:42 -0700

    header style updates

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 
   <nav class="navbar navbar-expand-lg bg-body-white">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/VistexDrsForYou"><img src="http://localhost/VistexDrsForYou/wp-content/uploads/2023/04/DFY-nav-logo.png" alt="DFY nav color logo"/></a>
+      <a class="navbar-brand" href="/VistexDrsForYou"><img src="http://localhost/VistexDrsForYou/wp-content/uploads/2023/04/DFY-nav-logo.png" width="150" height="127"  alt="DFY nav color logo"/></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
