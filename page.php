e790d53f7fcc3f70e0a895964705787b000b19e9
vistex-greyson <michelle.greyson@vistex.com>
2023-05-24 13:19:22 -0700

    style updates to mid-section

--- page.php
+++ page.php
@@ -32,28 +32,31 @@
 <section class="dfy-mid-section">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-4 offset-md-2">
                 <h3>Doctors For You International is an extended arm of Doctors For You India</h3>
                 <p>The organization is pioneer in public health care services to marginalized populations and a leader among entities for disaster relief response in the shortest period of time.</p>
                 <p class="dark-btn"><a class="btn btn-lg btn-dark text-white" href="#">learn more</a></p>
             </div>
             <div class="col-lg-2 icons-col">
-                <p><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/calendar.png" alt="calender image"></p>
-                <p><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/india.png" alt="india image"></p>
-                <p><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/location.png" alt="location-ping image"></p>
+                <p><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/calendar.png" width="50" alt="calender image"></p>
+                <p><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/india.png" width="50" alt="india image"></p>
+                <p><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/location.png" width="50" alt="location-ping image"></p>
             </div>
             <div class="col-lg-4">
                 <div>
                     <p class="pink-number">15</p>
                     <p class="grey-text">Number of <b>years operating</b> in India</p>
+                    <hr class="pink-line">
                 </div>
                 <div>
                     <p class="pink-number">3</p>
                     <p class="grey-text">The number of Union <b>territories spread</b> in India</p>
+                    <hr class="pink-line">
                 </div>
                 <div>
                     <p class="pink-number">25</p>
                     <p class="grey-text">The number of the <b>geographic footprint</b> spread of States in India</p>
+                    <hr class="pink-line">
                 </div>
             </div>
         </div>
