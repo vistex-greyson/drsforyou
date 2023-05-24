0f79db53bb7de9410e8b6bb1f51a324af5794403
vistex-greyson <michelle.greyson@vistex.com>
2023-05-24 09:14:06 -0700

    testing page.php

--- single.php
+++ single.php
@@ -1,24 +1,5 @@
 <?php get_header(); ?>
 
-<section class="dfy-homepage-hero">
-    <div class="container-fluid">
-        <div class="row">
-            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="https://drs.vistex.com/wp-content/uploads/2023/05/dfy-homepage-1-1.png" alt="First slide">
-                    </div>
-                    <div class="carousel-item new">
-                        <img class="d-block w-100" src="https://drs.vistex.com/wp-content/uploads/2023/05/dfy-homepage-2-1.png" alt="Second slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="https://drs.vistex.com/wp-content/uploads/2023/05/dfy-homepage-3-1.png" alt="Third slide">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
 
 
 <?php get_footer(); ?>
