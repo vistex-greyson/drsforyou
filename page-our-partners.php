3a053c83392f469d8b617d00687f0961de7d9e00
vistex-greyson <michelle.greyson@vistex.com>
2023-05-24 15:31:05 -0700

    adding new banner image to pages

--- page-our-partners.php
+++ page-our-partners.php
@@ -6,6 +6,9 @@ get_header();
 
 ?>
 
+<div class="dfy-header-image"> 
+    <img src="https://drs.vistex.com/wp-content/uploads/2023/05/dfy-header-banner.png">
+</div>
 
 
 
--- page-our-projects.php
+++ page-our-projects.php
@@ -7,7 +7,9 @@ get_header();
 ?>
 
 
-
+<div class="dfy-header-image"> 
+    <img src="https://drs.vistex.com/wp-content/uploads/2023/05/dfy-header-banner.png">
+</div>
 
 
 
