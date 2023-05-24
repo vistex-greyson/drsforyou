b85c0f149ef1a8943137ffe0d7275e2468029805
vistex-greyson <michelle.greyson@vistex.com>
2023-05-24 13:57:19 -0700

    style updates to mid-section 2

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
   <footer class="text-center text-white bg-dark">
     <div class="container-fluid" style="padding-top: 0px;">
       <section class="dfy-footer">
-        <div class="col-lg-12" style="display: flex; justify-content: center; position: relative; top: -115px;">
+        <div class="col-lg-12" style="display: flex; justify-content: center; position: relative; top: -115px; margin-top: 150px;">
           <a href="#"><img src="https://drs.vistex.com/wp-content/uploads/2023/05/footer-img-1.png" width="1000" alt="DFY footer img"></a>
         </div>
         <div class="row align-items-end pb-3">
