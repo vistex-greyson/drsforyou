586a441ce80708b3108efb3cdb640f2433d13fde
vistex-greyson <michelle.greyson@vistex.com>
2023-05-25 07:45:04 -0700

    code clean-up 3

--- functions.php
+++ functions.php
@@ -67,7 +67,6 @@ add_action( 'widgets_init', 'add_widget_support' );
 function dfy_scripts() {
 	wp_register_script('slickjs', get_template_directory_uri() . 'assets/js/slick-min.js', [], false, true);
 	wp_enqueue_script('slickjs');
-    wp_enqueue_script( 'popper-2','https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js', array( 'jquery' ),'',true );
 	wp_register_style('slick', get_template_directory_uri() . 'assets/css/slick.css', [], false, 'all');
 	wp_register_style('slick-theme', get_template_directory_uri() . 'assets/css/slick-theme.css', ['slick'], false, 'all');
 	wp_register_script('bootstrap-js', get_template_directory_uri() . 'assets/js/bootstrap.bundle.js', ['jquery'], false, true);
--- hero-carousel.php
+++ hero-carousel.php
@@ -6,13 +6,18 @@ DFY Hero Slider on the Homepage
 ?>
 
 <div class="dfy-hero-carousel">
-  <div>
-    <img src="https://drs.vistex.com/wp-content/uploads/2023/05/dfy-homepage-1-1.png" alt="dfy-1">
-  </div>
-  <div>
+    <div>
+        <img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/dfy-homepage-1-1.png" alt="dfy-1">
+        <div class="col-lg-9 offset-lg-3 dfy-hero-caption">
+            <h1 class="hero-title">Guided by humanitarian principles of humanity, impartiality, and neutrality.</h1>
+            <p class="hero-p">Providing sustainable, equitable, effective, and efficient health care services to the most vulnerable individuals and communities.</p>
+            <p class="hero-btn-outer"><a class="btn btn-lg btn-white hero-btn" href="#">donate now</a></p>
+        </div>
+    </div>
+  <!-- <div>
     <img src="https://drs.vistex.com/wp-content/uploads/2023/05/dfy-homepage-2-1.png" alt="dfy-2">
   </div>
   <div>
     <img src="https://drs.vistex.com/wp-content/uploads/2023/05/dfy-homepage-3-1.png" alt="dfy-3">
-  </div>
+  </div> -->
 </div>
