329f61aa09039216799de01febe947242140165f
vistex-greyson <michelle.greyson@vistex.com>
2023-05-25 11:56:25 -0700

    hero slider adjustments 3

--- page.php
+++ page.php
@@ -8,34 +8,6 @@
     <div class="container-fluid">
         <div class="row">
             <?php get_template_part('hero-carousel') ?>
-            <!-- <div id="dfyHeroCarousel" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="https://drs.vistex.com/wp-content/uploads/2023/05/dfy-homepage-1-1.png" alt="First slide">
-                        <div class="col-lg-9 offset-lg-3 dfy-hero-caption">
-                            <h1 class="hero-title">Guided by humanitarian principles of humanity, impartiality, and neutrality.</h1>
-                            <p class="hero-p">Providing sustainable, equitable, effective, and efficient health care services to the most vulnerable individuals and communities.</p>
-                            <p class="hero-btn-outer"><a class="btn btn-lg btn-white hero-btn" href="#">donate now</a></p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="https://drs.vistex.com/wp-content/uploads/2023/05/dfy-homepage-2-1.png" alt="Second slide">
-                        <div class="col-lg-9 offset-lg-3 dfy-hero-caption">
-                            <h1 class="hero-title">Guided by humanitarian principles of humanity, impartiality, and neutrality.</h1>
-                            <p class="hero-p">Providing sustainable, equitable, effective, and efficient health care services to the most vulnerable individuals and communities.</p>
-                            <p class="hero-btn-outer"><a class="btn btn-lg btn-white hero-btn" href="#">donate now</a></p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="https://drs.vistex.com/wp-content/uploads/2023/05/dfy-homepage-3-1.png" alt="Third slide">
-                        <div class="col-lg-9 offset-lg-3 dfy-hero-caption">
-                            <h1 class="hero-title">Guided by humanitarian principles of humanity, impartiality, and neutrality.</h1>
-                            <p class="hero-p">Providing sustainable, equitable, effective, and efficient health care services to the most vulnerable individuals and communities.</p>
-                            <p class="hero-btn-outer"><a class="btn btn-lg btn-white hero-btn" href="#">donate now</a></p>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
         </div>
     </div>
 </section>
