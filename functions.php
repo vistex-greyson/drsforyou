ad462e674b427d5fea47b01c0afbcffffb0bd343
vistex-greyson <michelle.greyson@vistex.com>
2023-05-24 17:59:15 -0700

    adding slick slider

--- functions.php
+++ functions.php
@@ -64,6 +64,18 @@ function add_widget_support() {
 // Hook the widget
 add_action( 'widgets_init', 'add_widget_support' );
 
-
+function dfy_scripts() {
+    wp_enqueue_style('bootstrap5-3', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css');
+    wp_enqueue_script( 'jquery-3','https://code.jquery.com/jquery-3.7.0.min.js', array( 'jquery' ),'',true );
+    wp_enqueue_script( 'popper-2','https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js', array( 'jquery' ),'',true );
+    wp_enqueue_script( 'bs-bundle','https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', array( 'jquery' ),'',true );
+	wp_register_style('slick', get_template_directory_uri() . 'assets/css/slick.css');
+	wp_register_style('slick-theme', get_template_directory_uri() . 'assets/css/slick-theme.css');
+	wp_register_script('slickjs', get_template_directory_uri() . 'assets/js/slick.js', ['jquery'], false, true);
+	wp_enqueue_script('slickjs');
+	wp_enqueue_style('slick');
+	wp_enqueue_style('slick-theme');
+}
+add_action( 'wp_enqueue_scripts', 'dfy_scripts' );
 	
 
