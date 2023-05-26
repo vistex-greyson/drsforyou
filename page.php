520c8d06d66d2ba567be09ec4588c38d393011cf
vistex-greyson <michelle.greyson@vistex.com>
2023-05-25 18:58:27 -0700

    style adjustments 4

--- page.php
+++ page.php
@@ -60,7 +60,7 @@
             <div class="col-lg-6">
                 <img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/support-img.png" width="65%" alt="">
             </div>
-            <div class="col-lg-6 align-items-center">
+            <div class="col-lg-6 align-self-center">
                 <h2><b>Any amount counts to Support Doctors For You</b></h2> 
                 <p class="grey-text">As a not for profit organization, Doctors For You seeks support from various organizations and individuals. Any support from small to big can make a difference in someone’s life.shortest period of time.</p>
                 <p class="mid-dark-btn"><a class="btn btn-lg btn-dark text-white dark-btn" href="#">donate now</a></p>
@@ -72,15 +72,15 @@
 <section class="recognitions-section">
     <div class="container">
         <div><h1 class="recog-title">Recognitions</h1></div>
-        <div class="row">
-            <div class="col-lg-4"><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/recog_1-1.png" width="50%" alt=""></div>
-            <div class="col-lg-4"><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/recog_1-2.png" width="50%" alt=""></div>
-            <div class="col-lg-4"><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/recog_1-3.png" width="50%" alt=""></div>
+        <div class="row justify-content-center">
+            <div class="col-lg-4 align-self-center"><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/recog_1-1.png" width="60%" alt=""></div>
+            <div class="col-lg-4 align-self-center"><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/recog_1-2.png" width="60%" alt=""></div>
+            <div class="col-lg-4 align-self-center"><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/recog_1-3.png" width="60%" alt=""></div>
         </div>
-        <div class="row">
-            <div class="col-lg-4"><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/recog_2-1.png" width="50%" alt=""></div>
-            <div class="col-lg-4"><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/recog_2-2.png" width="50%" alt=""></div>
-            <div class="col-lg-4"><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/recog_2-3.png" width="50%" alt=""></div>
+        <div class="row justify-content-center">
+            <div class="col-lg-4 align-self-center"><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/recog_2-1.png" width="60%" alt=""></div>
+            <div class="col-lg-4 align-self-center"><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/recog_2-2.png" width="60%" alt=""></div>
+            <div class="col-lg-4 align-self-center"><img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/recog_2-3.png" width="60%" alt=""></div>
         </div>
     </div>
 </section>
