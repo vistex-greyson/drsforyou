af014b982532656a992742b4c1e3d3d7ca7ca4cd
vistex-greyson <michelle.greyson@vistex.com>
2023-05-30 08:35:46 -0700

    realignment of project carousel slides

--- project-carousel.php
+++ project-carousel.php
@@ -10,43 +10,43 @@ DFY "Our Projects" Slider on the Homepage
         <h1 class="slider-title">Our Projects</h1>
         <div class="row justify-content-center">
             <div class="project-carousel" style="width: 90% !important">
-                <div class="col-lg-2" style="width: 350px !important" style="width: 350px !important">
+                <div class="col-lg-2 project-slide" style="width: 350px !important" style="width: 350px !important">
                     <img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/maternal.png" width="300" alt="maternal-img">
                     <p class="slider-text">Maternal & Child Health</p>
                 </div>
-                <div class="col-lg-2" style="width: 350px !important">
+                <div class="col-lg-2 project-slide" style="width: 350px !important">
                     <img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/nutrition.png" width="300" alt="nutrition-img">
                     <p class="slider-text">Nutrition Program</p>
                 </div>
-                <div class="col-lg-2" style="width: 350px !important">
+                <div class="col-lg-2 project-slide" style="width: 350px !important">
                     <img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/cancer-program.png" width="300" alt="cancer-img">
                     <p class="slider-text">Cancer Program</p>
                 </div>
-                <div class="col-lg-2" style="width: 350px !important">
+                <div class="col-lg-2 project-slide" style="width: 350px !important">
                     <img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/tuberculosis.png" width="300" alt="tuberculosis-img">
                     <p class="slider-text">Tuberculosis Program</p>
                 </div>
-                <div class="col-lg-2" style="width: 350px !important">
+                <div class="col-lg-2 project-slide" style="width: 350px !important">
                     <img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/skill.png" width="300" alt="skill-img">
                     <p class="slider-text">Skill Development</p>
                 </div>
-                <div class="col-lg-2" style="width: 350px !important">
+                <div class="col-lg-2 project-slide" style="width: 350px !important">
                     <img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/skill.png" width="300" alt="skill-img">
                     <p class="slider-text">Skill Development</p>
                 </div>
-                <div class="col-lg-2" style="width: 350px !important">
+                <div class="col-lg-2 project-slide" style="width: 350px !important">
                     <img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/skill.png" width="300" alt="skill-img">
                     <p class="slider-text">Skill Development</p>
                 </div>
-                <div class="col-lg-2" style="width: 350px !important">
+                <div class="col-lg-2 project-slide" style="width: 350px !important">
                     <img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/skill.png" width="300" alt="skill-img">
                     <p class="slider-text">Skill Development</p>
                 </div>
-                <div class="col-lg-2" style="width: 350px !important">
+                <div class="col-lg-2 project-slide" style="width: 350px !important">
                     <img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/skill.png" width="300" alt="skill-img">
                     <p class="slider-text">Skill Development</p>
                 </div>
-                <div class="col-lg-2" style="width: 350px !important">
+                <div class="col-lg-2 project-slide" style="width: 350px !important">
                     <img class="img-responsive" src="https://drs.vistex.com/wp-content/uploads/2023/05/skill.png" width="300" alt="skill-img">
                     <p class="slider-text">Skill Development</p>
                 </div>
